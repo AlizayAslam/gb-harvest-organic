c1907700f374bd7befeb0173352e8bbffbe5c70e
AlizayRBK <alizaaslamrbk@gmail.com>
2025-05-18 13:23:15 +0500

    set up for gb harvest organic  front end

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-green-500 text-white p-4">
+      Welcome to GB Harvest Organic
+    </div>
+  );
+}
+
+export default App;
